c72798d939b98fbaa19e4d79cd19d3c7fb752062
shenyiyin <2262193538@qq.com>
2021-02-11 09:17:13 +0800

    Add index page

--- flas-practice/app.py
+++ flas-practice/app.py
@@ -1,3 +1,4 @@
+#2021.2.10
 from flask import Flask,escape,url_for
 
 #__name__属性为当是本文件时为__main__，当调用时为该文件文件名
@@ -28,7 +29,26 @@ def test_url_for():
     print(url_for('user_page',name="syy"))
     print(url_for('user_page',name="yyf"))
     print(url_for('test_url_for'))
-
+    #该页面的返回值
     return 'Test_page'
+# 2.11
+from flask import render_template
+name = 'SYY'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+@app.route("/")
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
 if __name__=="__main__":
     app.run(debug=True)
